3c1f861c7387b45d4fa4bad29c2835d1f6fb6506
cor <cor@pruijs.nl>
2015-12-02 21:30:52 +0100

    Add contents of ios/ from woording/woording

---
+++ Pods/Target Support Files/Pods-wording/Pods-wording-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wording : NSObject
+@end
+@implementation PodsDummy_Pods_wording
+@end
---
+++ Pods/Target Support Files/Pods-wording/Pods-wording-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_wordingVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_wordingVersionString[];
+
