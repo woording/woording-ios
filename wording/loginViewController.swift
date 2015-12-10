a5df6660f817b88d9aed318bb4f1d416e4a25787
Justin Sandhu <justin@justins-imac.local>
2015-12-10 02:17:11 -0800

    added loginViewController outline

---
+++ wording/loginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  loginViewController.swift
+//  wording
+//
+//  Created by Justin on 12/10/15.
+//  Copyright © 2015 Cor Pruijs. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        //check here if user would like to be auto signed in
+        
+        
+        
+        // if (autologin == yes) {      continue login         }
+        
+        
+        
+        func validateCredentials(username username:String, password: String) {
+            
+            //do that JSON shit where you pass in the Dictionary of the creds 
+            
+            // if (login == sucessful) {   shouldSignIn() }
+            
+        }
+       
+    }
+
+}
