3be74f830ad757ae19ad9f5b56fd9d50fe121aea
Justin Sandhu <justin@justins-imac.local>
2016-01-08 01:11:46 -0800

    Building list inspector view

--- wording/LoginViewController.swift
+++ wording/LoginViewController.swift
@@ -14,9 +14,43 @@ class LoginViewController : UIViewController {
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
+    var username : String?
+    var password : String?
+    
+    
     @IBAction func loginButtonPressed(sender: AnyObject) {
-        let password: String? = passwordField.text
+         username  = usernameField.text
+         password  = passwordField.text
+        
         print(password)
     }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let controller = segue.destinationViewController as! MainViewController
+        
+        
+        if segue.identifier != nil { //check if there is an ID for the seg
+            
+        switch segue.identifier!{ //if there is an ID switch against these values
+        case "Login" :
+            print("this")
+            
+            controller.name = username!
+            
+            
+            
+            
+        case "that" : //never going to happen
+            print("that")
+        default :    //this either.
+            print("what")
+        }
+        }
+    }
+
+    
+    
+    
     
 }
--- wording/MainViewController.swift
+++ wording/MainViewController.swift
@@ -11,10 +11,13 @@ import RealmSwift
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   
     
-    var listsArchive : [String : [String:String]] = ["listName" : ["word1": "trans1", "word2" : "trans2"]]
-    var listNames : [String]
-    
+    var listsArchive : [String : [String:String]] = ["defaut" : ["word1": "trans1", "word2" : "trans2"]]
+    var listNames : [String] = []
+    var name : String = "" 
 
+
+    @IBOutlet weak var optionView: UIView!
+    @IBOutlet var nameLabel: UILabel!
     @IBOutlet weak var listTable: UITableView!
     
     
@@ -36,23 +39,36 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
+         let wordingService = WordingService()
         
-        let wordingService = WordingService()
+            self.nameLabel.text   = "Welcome, " + name
+            listsArchive.removeAll()
+        // cleans the archive for plist read
         
-        listTable.dataSource = self
+       
+        
+            listTable.dataSource = self
+            listTable.delegate = self
         
         //read Lists from Plist.
         //recreate listArchive from plist. 
         
-        // listArchive = [READ FROM PLIST]
         
         
         
-        
-        //assemple array of names for data table source
-        for list in listsArchive {
-            self.listNames.append( list.0 )
+        if let path = NSBundle.mainBundle().pathForResource("Lists", ofType: "plist"), dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] {
+            
+            for list in dict {
+                //assemple array of names for data table source
+
+                listNames.append( list.0 )
+            }
         }
+        
+        
+        
+        
+     
                
         
         //get list example
@@ -75,15 +91,32 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     
 //Table View 
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return lists.count
+     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+       let spot =  tableView.cellForRowAtIndexPath(indexPath)?.center
+        
+
+        UIView.animateWithDuration(0.5, animations: {
+            self.optionView.frame.origin = spot!
+            //self.optionView.frame.origin.x  = 140 + self.optionView.frame.origin.x
+        })
+    
     }
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listNames.count
+    }
+    
+    
+    
+    
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) ->   UITableViewCell {
         let cell = UITableViewCell()
         cell.backgroundColor = UIColor.clearColor()
         let label = UILabel(frame: CGRect(x:0, y:0, width:200, height:50))
-        label.text = self.lists.
+        label.text = "\t" + listNames[indexPath.item]
         cell.addSubview(label)
         return cell
     }
