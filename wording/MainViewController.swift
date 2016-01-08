6efd434d936057cd5686f353dcacaba29b19309b
Justin Sandhu <justin@justins-imac.local>
2016-01-07 20:43:55 -0800

    Table View Data Source

--- wording/MainViewController.swift
+++ wording/MainViewController.swift
@@ -7,25 +7,72 @@
 //
 
 import UIKit
-
-class MainViewController: UIViewController {
+import RealmSwift
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
     
-    
 
+    @IBOutlet weak var listTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let wordingService = WordingService()
         
-        wordingService.getUser("cor") {
-            json in
-            
+        listTable.dataSource = self
+        
+               
+        
+        //get list example
+        wordingService.getUser("cor", callback: {
+        json in
+        print(json)
         }
         
+        )
         
         
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+//Table View 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) ->   UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = UIColor.clearColor()
+        let label = UILabel(frame: CGRect(x:0, y:0, width:200, height:50))
+        label.text = "    Hello Man"
+        cell.addSubview(label)
+        return cell
+    }
+    
+    
+    // UITableViewDelegate Functions
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
